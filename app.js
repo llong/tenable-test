51cebc18cb778b89dcd7915e96a4cc6374bd6cdb
Lewis Long <llongdesigns@gmail.com>
2016-08-18 19:39:48 -0400

    added ajax functionality

--- app.js
+++ app.js
@@ -1,43 +1,16 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET','data.json', true);
-xhr.send();
+$(function(){
+  $.getJSON('data.json', function(data){
+    // Log each key in the response data
+    $.each( data.configurations, function( index, element ) {
+        $('#mainContainer').append(
+          '<div class="card">' +
+          '<p>' + element.name + '</p>' +
+          '<p>' + element.hostname + '</p>' +
+          '<p>' + element.port + '</p>' +
+          '<p>' + element.username + '</p>' + 
+          '</div>')
 
-xhr.onreadystatechange = processRequest;
-var response = [];
-
-function processRequest(e) {
-  if (xhr.readyState === 4 && xhr.status === 200){
-    var response = JSON.parse(xhr.responseText);
-    var configurations = response.configurations;
-
-    for(i=0; i< configurations.length;i++){
-      var card = document.createElement('div');
-      card.className = 'card';
-
-      function cardContent(text){
-        var P = document.createElement('P');
-        P.appendChild(document.createTextNode(text));
-        return P;
-      }
-
-      var values = [
-        cardContent(configurations[i].name),
-        cardContent(configurations[i].hostname),
-        cardContent(configurations[i].port),
-        cardContent(configurations[i].username)
-      ]
-
-      var docFrag = document.createDocumentFragment();
-        for(var x = 0; x < values.length; x++) {
-          docFrag.appendChild(values[x])
-        }
-
-
-
-      var container = document.getElementById('mainContainer');
-      card.appendChild(docFrag);
-      container.appendChild(card);
-      console.log(i);
-    }
-  }
-}
+        console.log( element.name);
+    });
+  })
+})
