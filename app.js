56712ba9e705b08e75aca2ac58defd8b050c626e
Lewis Long <llongdesigns@gmail.com>
2016-08-18 15:43:14 -0400

    created basic angular app

--- app.js
+++ app.js
@@ -1,43 +1,20 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET','data.json', true);
-xhr.send();
+var app = angular.module('configurationApp',[]);
 
-xhr.onreadystatechange = processRequest;
-var response = [];
+app.factory('configurations',['$http', function($http){
+  var obj = {};
 
-function processRequest(e) {
-  if (xhr.readyState === 4 && xhr.status === 200){
-    var response = JSON.parse(xhr.responseText);
-    var configurations = response.configurations;
-
-    for(i=0; i< configurations.length;i++){
-      var card = document.createElement('div');
-      card.className = 'card';
-
-      function cardContent(text){
-        var P = document.createElement('P');
-        P.appendChild(document.createTextNode(text));
-        return P;
-      }
-
-      var values = [
-        cardContent(configurations[i].name),
-        cardContent(configurations[i].hostname),
-        cardContent(configurations[i].port),
-        cardContent(configurations[i].username)
-      ]
-
-      var docFrag = document.createDocumentFragment();
-        for(var x = 0; x < values.length; x++) {
-          docFrag.appendChild(values[x])
-        }
-
-
-
-      var container = document.getElementById('mainContainer');
-      card.appendChild(docFrag);
-      container.appendChild(card);
-      console.log(i);
-    }
+  obj.data = function(){
+    return $http.get('data.json');
   }
-}
+
+  return obj;
+}]);
+
+app.controller('mainController',['$scope','configurations', function($scope,configurations){
+  configurations.data().success(function(res){
+    $scope.configurations = res.configurations;
+  })
+  .catch(function(err){
+    throw err;
+  });
+}]);
