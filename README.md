e8b5ad7c19746a815fd42dd8f6f70a5e7e3f210e
Lewis Long <llongdesigns@gmail.com>
2016-08-18 19:54:21 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tenable-test
 Test app from Tenable, includes raw JS, jQuery and AngularJs branches
+
+Answer to question #5
+
+Depending on the time it took to download the entire JSON response I would either download all 10,000 objects at once and use a loading animation while that was happening. Once the objects had been loaded I would use pagination to divide them into groups of 50 - 100 similar to many ecommerce websites.
+
+Alternatively if the response took longer than say 10 seconds to load I would create a function to make a request for the first 100 objects and then have that function request for another 100 objects after the user had scrolled to the bottom of the page. Similar to something like Tumblr.
--- app.js
+++ app.js
@@ -1,42 +1,15 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET','data.json', true);
-xhr.send();
+$(function(){
+  $.getJSON('data.json', function(data){
+    $.each( data.configurations, function( index, element ) {
+        $('#mainContainer').append(
+          '<div class="card">' +
+          '<p> ' + element.name + '</p>' +
+          '<p>' + element.hostname + '</p>' +
+          '<p>' + element.port + '</p>' +
+          '<p>' + element.username + '</p>' +
+          '</div>')
 
-xhr.onreadystatechange = processRequest;
-
-function processRequest(e) {
-  if (xhr.readyState === 4 && xhr.status === 200){
-    var response = JSON.parse(xhr.responseText);
-    var configurations = response.configurations;
-
-    for(i=0; i< configurations.length;i++){
-      var card = document.createElement('div');
-      card.className = 'card';
-
-      function cardContent(text){
-        var P = document.createElement('P');
-        P.appendChild(document.createTextNode(text));
-        return P;
-      }
-
-      var values = [
-        cardContent('Name: ' + configurations[i].name),
-        cardContent('Hostname: ' + configurations[i].hostname),
-        cardContent('Port: ' + configurations[i].port),
-        cardContent('Username: ' + configurations[i].username)
-      ]
-
-      var docFrag = document.createDocumentFragment();
-        for(var x = 0; x < values.length; x++) {
-          docFrag.appendChild(values[x])
-        }
-
-
-
-      var container = document.getElementById('mainContainer');
-      card.appendChild(docFrag);
-      container.appendChild(card);
-      console.log(i);
-    }
-  }
-}
+        console.log( element.name);
+    });
+  })
+})
